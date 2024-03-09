cf3ccc3beee3768e989f8bd5d662c6ee13a8f744
Yangsuhoe988 <didtnghl123@naver.com>
2024-03-09 23:26:41 +0900

    패링
    만들었어

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -3,42 +3,53 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 6f;//이동속도
-    bool isleft = false;
-    bool isright = false;
     public float dashSpeed = 8f;//대쉬거리
-    bool iswall = false;
-    bool isGround = false;
+
     public int jumpcount = 2;//점프횟수
     public float jump = 17f;//점프 힘
     public float jumpPower = 0.05f;//쭉 눌렀을 때 더 띄워지는 값
-    float jumpTime = 0f;
-    bool isjump = false;
+
     public GameObject wall;
     public GameObject Ground;
+    public GameObject Parrying;
+    public GameObject PerfectParrying;
+    float ParryingCount = 0.5f;//완벽한 패링 가능 시간 밑에 174줄에 있는 것도 바꿔야 함 똑같은 값으로
+
+    public Transform parent;//prefab부모지정
+    GameObject ParryingManager;
+    bool isparrying = false;
+    bool stop = false;
+    bool isleft = false;
+    bool isright = false;
+    bool iswall = false;
+    bool isGround = false;
+    float jumpTime = 0f;
+    bool isjump = false;
     Rigidbody2D rigid;
     void Awake()
     {
 
         rigid = GetComponent<Rigidbody2D>();
 
-    }
-    void Start()
-    {
 
     }
-
     void Update()
     {
 
         Move();
         Jump();
         avoid();
+        defense();
 
     }
 
     void Move()//좌우 이동
     {
         iswall = wall.GetComponent<iswall>().wallreach;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             isleft = true;
@@ -53,6 +64,10 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             isleft = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -75,7 +90,7 @@ public class Player : MonoBehaviour
     {
 
 
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C) && isparrying == false && rigid.velocity.x < 0.01f)
         {
             isGround = Ground.GetComponent<isGround>().Groundreach;//isGround 정보 받아오기
             if (jumpcount > 0)
@@ -96,7 +111,7 @@ public class Player : MonoBehaviour
             {
                 rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);//그 이후 쭉 눌렀을 때 증가하는 점프량
                 jumpTime = jumpTime + Time.deltaTime;
-                if (jumpTime > 0.25f) //0.5f는 0.5초간 누를 수 있음
+                if (jumpTime > 0.25f) //0.25f는 0.25초간 누를 수 있음
                 {
                     isjump = false;
                 }
@@ -111,7 +126,7 @@ public class Player : MonoBehaviour
     }
     void avoid()
     {
-        if (isGround == true && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true && isparrying == false)
         {
             if (isleft == true)
             {
@@ -131,6 +146,43 @@ public class Player : MonoBehaviour
         }
 
 
+    }
+    void defense()
+    {//막기 중에는 점프 밑 구르기 X 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isparrying = true;
+            speed = speed - 3f;//막기 시 감속
+
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            //Debug.Log(ParryingCount);
+            ParryingCount = ParryingCount - Time.deltaTime;
+            if (ParryingCount > 0 && stop == false)
+            {
+                ParryingManager = Instantiate(PerfectParrying, parent);
+                stop = true;
+            }
+            else if (ParryingCount < 0 && stop == true)
+            {
+                Debug.Log("가보자가보자~");
+                Destroy(ParryingManager);
+                ParryingManager = Instantiate(Parrying, parent);
+                stop = false;
+            }
+
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Destroy(ParryingManager);
+            isparrying = false;
+            speed = speed + 3f;
+            ParryingCount = 0.5f;//이 값을 바꿔야 함
+            stop = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -159,6 +211,7 @@ public class Player : MonoBehaviour
                 jumpTime = 5f;
                 jumpcount--;
                 //점프를 하지 않고 떨어졌을 때 점프키를 누르면 늦게떨어지는 것을 방지
+                rigid.velocity = new Vector2(0, rigid.velocity.y);
             }
 
         }
---
+++ Assets/script/defense.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defense : MonoBehaviour
+{
+    public float PerfectTime = 1f;
+    private void Awake()
+    {
+        Debug.Log("¸·±â@!");
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
