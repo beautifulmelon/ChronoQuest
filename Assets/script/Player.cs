5199f8baa22098bfe2fd4abe63b4677761ec5091
Yangsuhoe988 <didtnghl123@naver.com>
2024-03-13 01:17:24 +0900

    이동 구체화
    자잘버그해결

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
     public float speed = 6f;//이동속도
     public float dashSpeed = 8f;//대쉬거리
 
-    public int jumpcount = 0;//점프횟수
+    public int jumpcount = 2;//점프횟수
     public float jump = 17f;//점프 힘
     public float jumpPower = 0.05f;//쭉 눌렀을 때 더 띄워지는 값
 
@@ -18,17 +18,15 @@ public class Player : MonoBehaviour
     bool attackOn = true;
     public Transform parent;//prefab부모지정
     GameObject attackManager;
-    bool isparrying = false;
-    bool isleft = false;
-    bool isright = false;
     bool iswall = false;
     bool isGround = false;
     float jumpTime = 0f;
     bool isjump = false;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
     void Awake()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
 
 
@@ -40,7 +38,18 @@ public class Player : MonoBehaviour
         Jump();
         avoid();
         attack();
-
+        if(iswall == true && isGround == false)
+        {
+            //벽타기
+            if(rigid.velocity.y < -0.5f)//0.5는 maxspeed
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, -0.5f);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        
     }
 
     void Move()//좌우 이동
@@ -52,59 +61,51 @@ public class Player : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            isleft = true;
-            isright = false;
 
             if (iswall == false)
             {
                 transform.Translate(-speed * Time.deltaTime, 0, 0);//벽에 안 닿았을 때만 이동가능
             }
-            transform.localScale = new Vector3(-1, 1, 1);//방향전환
+            transform.localScale = new Vector2(-1, 1);//방향전환
         }
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            isleft = false;
-        }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             rigid.velocity = new Vector2(0, rigid.velocity.y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            isright = true;
-            isleft = false;
+
             if (iswall == false)
             {
                 transform.Translate(speed * Time.deltaTime, 0, 0);
             }
 
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector2(1, 1);
         }
-        if (Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            isright = false;
-        }
+
         if (isGround == true)
         {
-            jumpcount = 1;
+            jumpcount = 2;
         }
 
 
     }
+
     void Jump()
     {
-
-
-        if (Input.GetKeyDown(KeyCode.C) && isparrying == false)
+        isGround = Ground.GetComponent<isGround>().Groundreach;
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            isGround = Ground.GetComponent<isGround>().Groundreach;//isGround 정보 받아오기
-            if (jumpcount < 2)
-            {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
 
+            if (jumpcount > 0)
+            {
                 rigid.velocity = new Vector2(rigid.velocity.x, 0);
                 rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);//점프   
                 isjump = true;
-                jumpcount++;
+                jumpcount--;
+
 
             }
 
@@ -126,53 +127,59 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.C))//점프를 살짝만 누르고 땠을 때 다시 누르면 힘을 줄 수 있던 것을 방지
         {
             isjump = false;
+
         }
-        isGround = Ground.GetComponent<isGround>().Groundreach;
+
         if (isGround == true)
         {
             jumpTime = 0f;
-            isjump = false;
-            jumpcount = 0;
+            jumpcount = 2;
+
         }
         if (isGround == false && isjump == false)
         {
-            jumpTime = 5f;
-            if (jumpcount == 0)
+            //jumpTime = 5f;
+            if (jumpcount == 2)
             {
                 jumpcount = 1;
             }
-            //점프를 하지 않고 떨어졌을 때 점프키를 누르면 늦게떨어지는 것을 방지
-            rigid.velocity = new Vector2(0, rigid.velocity.y);
+            //점프를 하지 않고 떨어졌을 때 점프키를 누르면 2번점프되는걸 방지
+            
         }
 
     }
     void avoid()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround == true && isparrying == false)
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
         {
-            if (isleft == true)
+            if(iswall == false)
             {
-                rigid.velocity = new Vector2 (-dashSpeed, rigid.velocity.y);
-                //rigid.AddForce(Vector2.left * dashSpeed, ForceMode2D.Impulse);
+                //여기에 구르기 모션만 넣기
+                if (transform.localScale.x == -1)
+                {
+                    rigid.velocity = new Vector2(-dashSpeed, rigid.velocity.y);
 
+
+                }
+                else if (transform.localScale.x == 1)
+                {
+                    rigid.velocity = new Vector2(dashSpeed, rigid.velocity.y);
+                    //rigid.AddForce(Vector2.right * dashSpeed, ForceMode2D.Impulse);
+
+                }
             }
-            else if (isright == true)
-            {
-                rigid.velocity = new Vector2(dashSpeed, rigid.velocity.y);
-                //rigid.AddForce(Vector2.right * dashSpeed, ForceMode2D.Impulse);
 
-            }
 
+            
+
+            //애니메이션으로 굴렀을 때 무적 상태 넣기(구르기 모션일때 무적) + 구르기 쿨타임 + 구르기 모션일 때 방향전환 x
         }
-        if (rigid.velocity.x > 0)
-        {
-            //여기에 구르기 시 무적상태 넣기
-        }
+
 
 
     }
     void attack()
-    {//막기 중에는 점프 밑 구르기 X 
+    {
 
         if (Input.GetKeyDown(KeyCode.X) && attackOn == true)
         {
@@ -209,7 +216,6 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)//자식 및 본인 모든 콜라이더에게 적용
     {
 
-        Debug.Log("아무거나 닿음");
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
--- Assets/script/attack.cs
+++ Assets/script/attack.cs
@@ -7,7 +7,7 @@ public class attack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("»ý¼ºµÊ");
+    
     }
 
     // Update is called once per frame
