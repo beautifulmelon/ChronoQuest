a5ca1d5eeef8453a0f3fb2c65594227ca4b8b5d1
Yangsuhoe988 <didtnghl123@naver.com>
2024-03-14 00:48:40 +0900

    Enemy공격
    .

---
+++ Assets/script/Player/attack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("떄림");
+            //죽이기
+        }
+        if(collision.gameObject.tag == "EnemyAttack")
+        {
+            //패링
+        }
+    }
+}
