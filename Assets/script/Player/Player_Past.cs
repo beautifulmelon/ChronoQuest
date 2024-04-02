4357905a0f1b1826808669fb5ba7b0fcd5f7ef59
Yangsuhoe988 <didtnghl123@naver.com>
2024-04-03 01:11:11 +0900

    d
    d

--- Assets/script/Player/Player_Past.cs
+++ Assets/script/Player/Player_Past.cs
@@ -1,51 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Past : MonoBehaviour
 {
     public Transform Player;
-    private Vector3[] pos = new Vector3[360];
-    int i = 0;
-    int x = 0;
+
+    private Vector3[] pos = new Vector3[720]; // 더 나중의 위치를 가져오고 싶다면 이걸 높게
+    private int currentIndex = 0;
+    public int framesToGoBack; // 뒤로 이동할 프레임 수
+    int previousIndex = 0;
+    float recordInterval = 0;
     private void Start()
     {
-        for (int j = 0; j < 360; j++)
+        // 초기 위치 기록
+        for (int j = 0; j < 720; j++)
         {
-            pos[j] = Player.transform.position;
+            pos[j] = Player.position;
         }
     }
+
     private void Update()
     {
-        //pastTime = pastTime + Time.deltaTime;
-        
-        
+        recordInterval = recordInterval + Time.deltaTime;
+        if(recordInterval > 0.01f)   
+        {
+            recordInterval = 0;
+            currentIndex = (currentIndex + 1) % pos.Length;
+            pos[currentIndex] = Player.position;
+            previousIndex = (currentIndex - framesToGoBack + pos.Length) % pos.Length;
+            transform.position = pos[previousIndex];
+        }
+        Debug.Log(Time.deltaTime);  
+        // 현재 위치 기록
 
-        pos[i] = Player.transform.position;//매 프레임 저장 위치 저장
-        i++;
-        
 
-        
-        if(i >= 360)
+        // A 키를 누르면 framesToGoBack 만큼 이전의 위치로 이동
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            i = 0;
+            // 현재 인덱스에서 framesToGoBack 만큼 뒤로 이동하여 이전 위치를 가져옴
+            
+            Player.position = pos[previousIndex];
         }
-        if(i > 200 ) 
-        {
-            x = i - 200;
-            transform.position = pos[x];
-        }
-        else
-        {
-            x = (i + 160) % 360;
-            transform.position = pos[x];
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Player.transform.position = pos[x];
-        }
-      
-
-        
     }
 }
